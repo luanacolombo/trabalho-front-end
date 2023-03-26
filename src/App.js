99454af392214a17307867d1e371302f7f2e8461
luanacolombo <luu_colombo@hotmail.com>
2023-03-26 19:29:59 -0300

    removido rodap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 //import Toast from 'react-bootstrap/Toast';
 import Button from 'react-bootstrap/Button';
-import {  Container, Form, Image, Row } from 'react-bootstrap';
+import { Container, Form, Image, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -21,54 +21,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 };*/
 
 const App = () => (
-  <div className='container-fluid p-0' style={{backgroundColor: "#eadaea"}}>
+  <div className='container-fluid p-0' style={{ backgroundColor: "#eadaea" }}>
     <Container>
-    <Row>
-      <div className="mx-auto col-md-4 d-flex vh-100">
-        <main className="form-signin w-100 m-auto">
-          <Form>
-            <Image className="mb-4 img-thumbnail" src="/logo.jpeg"></Image>
-            <h1 className="h3 mb-3 fw-normal text-center">Acesse sua conta</h1>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="E-mail" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Senha" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Lembrar-me" />
-            </Form.Group>
-            <div style={{display:"flex", flexDirection:"row", justifyContent:"space-around"}}>
-            <Button variant='' className="btn btn-outline-dark" type="submit">
-              Confirmar
-            </Button>
-            <Button variant='' className="btn btn-outline-dark" type="submit">
-              Esqueci minha senha
-            </Button>
-            <Button variant='' className="btn btn-outline-dark" type="submit">
-              Criar conta
-            </Button>
-            </div>
-          </Form>
-        </main>
-      </div>
-    </Row>
-    </Container>
-    <footer style={{bottom:0, position:"absolute",width: "100%"}} className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div className="col-md-4 d-flex align-items-center">
-          <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-            <svg className="bi" width="30" height="24"></svg>
-          </a>
-          <span className="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+      <Row>
+        <div className="mx-auto col-md-4 d-flex vh-100">
+          <main className="form-signin w-100 m-auto">
+            <Form>
+              <Image className="mb-4 img-thumbnail" src="/logo.jpeg"></Image>
+              <h1 className="h3 mb-3 fw-normal text-center">Acesse sua conta</h1>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="E-mail" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Senha" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Lembrar-me" />
+              </Form.Group>
+              <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
+                <Button variant='' className="btn btn-outline-dark" type="submit">
+                  Entrar
+                </Button>
+                <Button variant='' className="btn btn-outline-dark" type="submit">
+                  Esqueci minha senha
+                </Button>
+                <Button variant='' className="btn btn-outline-dark" type="submit">
+                  Criar conta
+                </Button>
+              </div>
+            </Form>
+          </main>
         </div>
+      </Row>
+    </Container>
 
-        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li className="ms-3"><a className="text-muted" href="#"></a></li>
-          <li className="ms-3"><a className="text-muted" href="#"></a></li>
-          <li className="ms-3"><a className="text-muted" href="#"></a></li>
-        </ul>
-      </footer>
-      </div>
+  </div>
 );
 
 export default App;
